655ab64e97e11fe3d61db1ed09f75fd97a705cb3
webdaniu <webdaniu@gmail.com>
2017-04-20 14:04:14 +0800

    例行push
    没有异常

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tebItem">商品</div>
+      <div class="tebItem">评论</div>
+      <div class="tebItem">商家</div>
+    </div>
+    <div class="content">i am content</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import header from './components/header/header.vue';
+
+export default{
+  components: {
+    'v-header': header
+  }
+};
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus">
+  #app
+    .tab
+      display flex
+      height 40px
+      line-height 40px
+      width 100%
+      .tebItem
+        flex 1
+        text-align center
+</style>
