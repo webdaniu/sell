90bd35d15b60f4cfe7bca1927ab70f2b08e50c98
webdaniu <webdaniu@gmail.com>
2017-04-21 21:04:11 +0800

    前后端交互
    1.添加vue-resource插件在package.json
    2.在main.js中引入并且注册

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,11 @@
   import header from './components/header/header.vue'
 
   export default{
+    data() {
+      return {
+        seller: {}
+      }
+    },
     components: {
       'v-header': header
     }
@@ -33,14 +38,14 @@
     width 100%
     height 40px
     line-height 40px
-    border-1px(rgba(7,17,27,0.1))
+    border-1px(rgba(7, 17, 27, 0.1))
     .tebItem
       flex 1
       text-align center
       a
         display block
-        color rgb(77,85,93)
+        color rgb(77, 85, 93)
         font-size 28px
         &.v-link-active
-          color rgb(240,20,20)
+          color rgb(240, 20, 20)
 </style>
