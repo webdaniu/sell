b8f47a0592becc4c7b67b8dcb99f5f83cad4ba4e
webdaniu <webdaniu@gmail.com>
2017-04-29 22:49:47 +0800

    1.垂直居中使用table 2.flex左侧固定宽度右侧自适应

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="goods">
     <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type > 0" :class="classMap[item.type]" class="icon"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
     </div>
     <div class="foods-wrapper"></div>
   </div>
@@ -20,6 +28,7 @@
       }
     },
     created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
       this.$http.get('/api/goods').then((responce) => {
         responce = responce.body
         if (responce.errno === ERR_OK) {
@@ -31,6 +40,8 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .goods
     display flex
     position absolute
@@ -42,6 +53,37 @@
       flex 0 0 80px
       width 80px
       background-color #f3f5f7
+      .menu-item
+        display table
+        width 56px
+        height 54px
+        padding 0 12px
+        line-height 14px
+        .icon
+          display inline-block
+          vertical-align middle
+          width 12px
+          height 13px
+          margin-right 2px
+          background-size 12px 12px
+          background-repeat no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display table-cell
+          width 56px
+          border-1px(rgba(7,17,27,0.1))
+          vertical-align middle
+          font-size 12px
+
     .foods-wrapper
       flex 1
 </style>
