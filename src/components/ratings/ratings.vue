7230b52332304374ac55a99945e314673b04646b
webdaniu <webdaniu@gmail.com>
2017-05-16 22:34:26 +0800

    路由评论结构样式——评价列表
    1.结构样式
    2.评论列表切换和内容过滤

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -34,7 +34,7 @@
       <!--评价列表-->
       <div class="rating-wrapper">
         <ul>
-          <li class="rating-item" v-for="rating in ratings">
+          <li class="rating-item border-1px" v-for="rating in ratings" v-show="needShow(rating.rateType,rating.text)">
             <div class="avatar">
               <img :src="rating.avatar" width="28" height="28">
             </div>
@@ -47,9 +47,9 @@
                 <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
               </div>
               <p class="text">{{rating.text}}</p>
-              <div class="recomment" v-show="rating.recomment && rating.recomment.length">
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
                 <span class="icon-thumb_up"></span>
-                <span v-for="item in rating.recomment">{{item}}</span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
               </div>
               <div class="time">
                 {{rating.rateTime | formatDate}}
@@ -99,6 +99,32 @@
         }
       })
     },
+    methods: {
+      needShow(type, text) {
+        if (this.onlyContent && !text) {
+          return false
+        }
+        if (this.selectType === ALL) {
+          return true
+        } else {
+          return type === this.selectType
+        }
+      }
+    },
+    events: {
+      'ratingtype.select'(type) {
+        this.selectType = type
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent
+        this.$nextTick(() => {
+          this.scroll.refresh()
+        })
+      }
+    },
     filters: {
       formatDate(time) {
         let date = new Date(time)
@@ -114,6 +140,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .ratings
     position absolute
     top 174px
@@ -182,5 +209,65 @@
           .delivery
             margin-left 12px
             font-size 12px
+            color rgb(147, 153, 159)
+    .rating-wrapper
+      padding 0 18px
+      .rating-item
+        display flex
+        padding 18px 0
+        border-1px(rgba(7, 17, 27, 0.1))
+        .avatar
+          flex 0 0 28px
+          width 28px
+          margin-right 12px
+          img
+            border-radius 50%
+        .content
+          position relative
+          flex 1
+          .name
+            margin-bottom 4px
+            line-height 12px
+            font-size 10px
+            color rgb(7, 17, 27)
+          .star-wrapper
+            margin-bottom 6px
+            font-size 0
+            .star
+              display inline-block
+              vertical-align top
+              margin-right 6px
+            .delivery
+              display inline-block
+              vertical-align top
+              line-height 12px
+              font-size 10px
+              color rgb(147, 153, 159)
+          .text
+            margin-bottom 8px
+            line-height 18px
+            color rgb(7, 17, 27)
+            font-size 12px
+          .recommend
+            line-height 16px
+            font-size 0
+            .icon-thumb_up, .item
+              display inline-block
+              margin 0 8px 4px 0
+              font-size 9px
+            .icon-thumb_up
+              color rgb(0, 160, 220)
+            .item
+              padding 0 6px
+              border 1px solid rgba(7, 17, 27, 0.1)
+              border-radius 1px
+              color rgb(147, 153, 159)
+              background #fff
+          .time
+            position absolute
+            top 0
+            right 0
+            line-height 12px
+            font-size 10px
             color rgb(147, 153, 159)
 </style>
